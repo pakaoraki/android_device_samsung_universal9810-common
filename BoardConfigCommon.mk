0ea0864ed9f23e84e950fdffe2ea3c4500b49dd5
pakaoraki <pakaoraki@gmx.com>
2021-06-10 22:48:13 +0200

    jf-common: Remove USE_XML_AUDIO_POLICY_CONF
    * Dead since Q, where XML audio policies weren't enforced yet \n https://review.lineageos.org/q/I302a0ed97964b052176dc3bb0141db1ba85e78c8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,6 @@ COMMON_PATH := device/samsung/universal9810-common
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 
 # Audio
-USE_XML_AUDIO_POLICY_CONF := 1
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
